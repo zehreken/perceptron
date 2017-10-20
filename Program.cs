6543fa66d272b9452927e8eb396ff76ec8161c90
zehreken <zehreken@gmail.com>
2017-10-20 13:10:37 +0300

    Add initial files

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace perceptron
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+		}
+	}
+}
---
+++ Source/Simple.cs
@@ -0,0 +1,35 @@
+﻿namespace perceptron.Source
+{
+	public class Simple
+	{
+		private float[] weights;
+		private float outputLayer;
+		private const float learningRate = 0.1f;
+		private float threshold = 0.5f;
+
+		public Simple()
+		{
+			weights = new float[2];
+			outputLayer = 0f;
+		}
+
+		public void Step()
+		{
+			
+		}
+
+		public void Train(int[] input, int desiredValue)
+		{
+			float sum = 0f;
+			for (int i = 0; i < weights.Length; i++)
+			{
+				sum += input[i] * weights[i];
+			}
+
+			if (sum >= threshold)
+			{
+				
+			}
+		}
+	}
+}
