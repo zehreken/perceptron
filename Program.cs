b38033cd6a93492a78c9dcc2329403771181cad3
zehreken <zehreken@gmail.com>
2017-10-23 12:40:55 +0300

    Add new method calls

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace perceptron
 
 		private static void TrainAndUseAndGate()
 		{
-			Console.WriteLine("AND Gate");
+			Console.WriteLine("Step AND Gate");
 			var andGate = new StepPerceptron();
 			andGate.Train(new [] {0, 0}, 0, 100);
 			andGate.Train(new [] {0, 1}, 0, 100);
@@ -28,8 +28,36 @@ namespace perceptron
 		
 		private static void TrainAndUseOrGate()
 		{
-			Console.WriteLine("OR Gate");
+			Console.WriteLine("Step OR Gate");
 			var orGate = new StepPerceptron();
+			orGate.Train(new [] {0, 0}, 0, 100);
+			orGate.Train(new [] {0, 1}, 1, 100);
+			orGate.Train(new [] {1, 0}, 1, 100);
+			orGate.Train(new [] {1, 1}, 1, 100);
+			Console.WriteLine(orGate.Use(new[] {0, 0}));
+			Console.WriteLine(orGate.Use(new[] {0, 1}));
+			Console.WriteLine(orGate.Use(new[] {1, 0}));
+			Console.WriteLine(orGate.Use(new[] {1, 1}));
+		}
+
+		private static void TrainAndUseAndGateSigmoid()
+		{
+			Console.WriteLine("Sigmoid AND Gate");
+			var andGate = new SigmoidPerceptron();
+			andGate.Train(new [] {0, 0}, 0, 100);
+			andGate.Train(new [] {0, 1}, 0, 100);
+			andGate.Train(new [] {1, 0}, 0, 100);
+			andGate.Train(new [] {1, 1}, 1, 100);
+			Console.WriteLine(andGate.Use(new[] {0, 0}));
+			Console.WriteLine(andGate.Use(new[] {0, 1}));
+			Console.WriteLine(andGate.Use(new[] {1, 0}));
+			Console.WriteLine(andGate.Use(new[] {1, 1}));
+		}
+
+		private static void TrainAndUseOrGateSigmoid()
+		{
+			Console.WriteLine("Sigmoid OR Gate");
+			var orGate = new SigmoidPerceptron();
 			orGate.Train(new [] {0, 0}, 0, 100);
 			orGate.Train(new [] {0, 1}, 1, 100);
 			orGate.Train(new [] {1, 0}, 1, 100);
