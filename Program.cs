4a3591dbaf1722d6f2830a0f4c7e9fe14e160c2c
zehreken <zehreken@gmail.com>
2017-11-03 14:49:18 +0300

    Update TrainAndUseANN method

--- Program.cs
+++ Program.cs
@@ -8,22 +8,22 @@ namespace perceptron
 	{
 		public static void Main(string[] args)
 		{
-			TrainAndUseANN(new StepPerceptron(), Utility.trainingInputSet, Utility.outputAND);
-			TrainAndUseANN(new StepPerceptron(), Utility.trainingInputSet, Utility.outputOR);
+//			TrainAndUseANN(new StepPerceptron(), Utility.trainingInputSet, Utility.outputAND, 100);
+//			TrainAndUseANN(new StepPerceptron(), Utility.trainingInputSet, Utility.outputOR, 100);
 			
-			TrainAndUseANN(new SigmoidPerceptron(), Utility.trainingInputSet, Utility.outputOR);
-			TrainAndUseANN(new SigmoidPerceptron(), Utility.trainingInputSet, Utility.outputOR);
+			TrainAndUseANN(new SigmoidPerceptron(), Utility.trainingInputSet, Utility.outputAND, 1000);
+//			TrainAndUseANN(new SigmoidPerceptron(), Utility.trainingInputSet, Utility.outputOR, 1000);
 			
-			TrainAndUseANN(new BiasedPerceptron(), Utility.trainingInputSet, Utility.outputOR);
-			TrainAndUseANN(new BiasedPerceptron(), Utility.trainingInputSet, Utility.outputOR);
+//			TrainAndUseANN(new BiasedPerceptron(), Utility.trainingInputSet, Utility.outputAND, 100);
+//			TrainAndUseANN(new BiasedPerceptron(), Utility.trainingInputSet, Utility.outputOR, 100);
 			
-			TrainAndUseANN(new XorGate(), Utility.trainingInputSet, Utility.outputXOR);
+//			TrainAndUseANN(new XorGate(), Utility.trainingInputSet, Utility.outputXOR, 100);
 		}
 
-		private static void TrainAndUseANN(INeuralNetwork ann, List<int[]> trainingInputSet, int[] outputSet)
+		private static void TrainAndUseANN(INeuralNetwork ann, List<int[]> trainingInputSet, int[] outputSet, int stepCount)
 		{
 			var random = new Random(0);
-			for (int i = 0; i < 100; i++)
+			for (int i = 0; i < stepCount; i++)
 			{
 				int rnd = random.Next(0, 4);
 				ann.Train(trainingInputSet[rnd], outputSet[rnd]);
