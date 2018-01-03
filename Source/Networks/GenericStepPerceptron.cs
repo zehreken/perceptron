64fe2ac4510f15bc7876032731cc93b96865b42c
zehreken <zehreken@gmail.com>
2018-01-03 11:02:49 +0300

    Add generic step perceptron, will implement later

---
+++ Source/Networks/GenericStepPerceptron.cs
@@ -0,0 +1,15 @@
+﻿namespace perceptron.Source
+{
+	public class GenericStepPerceptron : INeuralNetwork
+	{
+		public bool Train(int[] input, int desiredValue)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public double Use(int[] input)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
